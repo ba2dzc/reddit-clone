8a20a2a39f56f8f7aacc0dba30dddbd47b398782
ba2dzc <batu.duzcu@gmail.com>
2023-09-26 19:26:49 +0300

    components added

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TechComponent } from './tech/tech.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
+  declarations: [			
     AppComponent,
-    TechComponent
-  ],
+    TechComponent,
+      NavbarComponent,
+      SidebarComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     HttpClientModule
